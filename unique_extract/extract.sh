55478a4ded43a68309d94ffd5ada951dd7a6bb1f
tmlwinstone <tmlwinstone@gmail.com>
2015-10-06 15:45:36 -0600

    Changed to superior script :)

--- unique_extract/extract.sh
+++ unique_extract/extract.sh
@@ -1,4 +1,4 @@
 # Iain George, October 6, 2015
 # This script will extract unique values from column of csv file.
 
-cut -f 2 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
+cut -f $1 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
