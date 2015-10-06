ef5de6de353913662ea816fa7fb721105345ea43
iaingeorge <theiguana@gmail.com>
2015-10-06 16:01:38 -0600

    Sean is a steely eyed trouble maker

--- unique_extract/extract.sh
+++ unique_extract/extract.sh
@@ -2,3 +2,5 @@
 # This script will extract unique values from column of csv file.
 
 cut -f $1 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
+
+what the what, where is sean when you need him!
